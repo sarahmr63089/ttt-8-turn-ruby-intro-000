153cf46388ae9bd912694466fcbf5594950f76cc
Sarah Rose <sarahrose63089@gmail.com>
2020-02-23 21:54:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,6 +40,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, char = "X")
-  else 
+  else !valid_move?(board, index)
+    turn(board)
   end
 end
